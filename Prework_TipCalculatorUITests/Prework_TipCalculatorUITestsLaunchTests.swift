106c51d7e8b6e1846bb7e22726fb162a32da19eb
Thomaswang0822 <thomaswang0822@outlook.com>
2021-12-31 13:12:07 -0800

    Initial Commit

---
+++ Prework_TipCalculatorUITests/Prework_TipCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Prework_TipCalculatorUITestsLaunchTests.swift
+//  Prework_TipCalculatorUITests
+//
+//  Created by Thomas Wang on 2021/12/31.
+//
+
+import XCTest
+
+class Prework_TipCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
